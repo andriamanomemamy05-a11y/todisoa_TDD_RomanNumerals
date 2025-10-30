9edfc210450b2d8a49c070925664430551a7d9ea
andriamanomemamy05-a11y <andriamanomemamy05@gmail.com>
2025-10-30 15:31:13 +0100

    First test that fails: RED

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Initialisation du projet et "First commit" dans le repos
 2. Ajout de l'arborescence
+3. Premier test qui échoue : ❌ (RED) — car le fichier toRomanNumeral n’existe pas encore dans le chemin indiqué.
---
+++ tests/ShouldConvertNumberToRoman.test.js
@@ -0,0 +1,13 @@
+const { toRoman } = require('../src/roman');
+
+test('convertit 1 en I', () => {
+  expect(toRomanNumeral(1)).toBe('I');
+});
+
+test('convertit 2 en II', () => {
+  expect(toRomanNumeral(2)).toBe('II');
+});
+
+test('convertit 3 en III', () => {
+  expect(toRomanNumeral(3)).toBe('III');
+});
