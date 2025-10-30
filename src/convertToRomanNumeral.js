a163a47817bd29b24f02d77d90facff7ec8c9a82
andriamanomemamy05-a11y <andriamanomemamy05@gmail.com>
2025-10-30 16:19:42 +0100

    Refactor , clean and comment conversion to number numeral

--- src/convertToRomanNumeral.js
+++ src/convertToRomanNumeral.js
@@ -1,22 +1,26 @@
 function convertToRomanNumeral(n) {
-    // Ajout et vérification des éxceptions pour les entrées invalides
-    // Vérifier que l'entrée est un nombre
-    if (typeof n !== 'number') {
-    throw new TypeError('Input must be a number');
-    }
+    // Vérifie que le nombre est dans la plage 1–3999
+    if (n <= 0 || n >= 4000) throw new RangeError('Out of range');
 
-    // Vérifier que l'entrée est un entier
-    if (!Number.isInteger(n)) {
-    throw new TypeError('Input must be an integer');
-    }
+    // Table de correspondance pour les cas simples (unités)
+    const numerals = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+    };
 
-    // Vérifier que l'entrée est dans la plage valide (1-3)
-    if (n <= 0 || n >= 4) {
-    throw new RangeError('Number must be between 1 and 3');
-    }
+    // Test si le nombre correspond directement à une clé du dictionnaire
+    if (numerals[n]) return numerals[n];
 
-    // Conversion basique pour les nombres 1 à 3 en I, II, III
-    return 'I'.repeat(n);
+    // Test si le nombre est entre 2 et 3 → répète 'I'
+    if (n < 4) return 'I'.repeat(n);
+
+     // Test si le nombre est entre 6 et 8 → 'V' suivi de 'I' répété
+    if (n < 9) return 'V' + 'I'.repeat(n - 5);
+
+    // Sinon , on affiche erreur pour les autres cas non gérés
+    throw new Error('Not implemented');
 }
 
-module.exports = { convertToRomanNumeral }; 
+module.exports = { convertToRomanNumeral };
--- tests/ShouldConvert4And9ToRoman.test.js
+++ tests/ShouldConvert4And9ToRoman.test.js
@@ -1,9 +1,9 @@
-const { convert4And9ToRoman } = require('../src/convert4And9ToRoman');
+const { convertToRomanNumeral } = require('../src/convertToRomanNumeral');
 
 test('convertit 4 en IV', () => {
-  expect(convert4And9ToRoman(4)).toBe('IV');
+  expect(convertToRomanNumeral(4)).toBe('IV');
 });
 
 test('convertit 9 en IX', () => {
-  expect(convert4And9ToRoman(9)).toBe('IX');
+  expect(convertToRomanNumeral(9)).toBe('IX');
 });
